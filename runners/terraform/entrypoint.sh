4bd61453bddcdc45f30956e3c5faa213956652df
capson12 <rualoguh@gmail.com>
2025-12-05 11:53:37 +0000

    new format

---
+++ runners/terraform/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Check if the required arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <repo_name> <repo_token>"
+    exit 1
+fi
+
+REPO_NAME=$1
+GITHUB_TOKEN=$2
+GITHUB_URL="https://github.com/Capson12/$REPO_NAME"
+
+# Configure the runner using expect
+expect << EOF
+spawn ./config.sh --url $GITHUB_URL --token $GITHUB_TOKEN
+
+send -- "\r"
+send -- "\r"
+send -- "\r"
+send -- "\r"
+
+expect eof
+EOF
+
+# Start the runner
+./run.sh
+
