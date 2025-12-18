2dd1daeb422ea35f80f0a6167db2f8658e321883
capson12 <rualoguh@gmail.com>
2025-12-18 08:08:42 +0000

    new format

--- run-me.sh
+++ run-me.sh
@@ -8,20 +8,8 @@ read -p "Enter Repository Token, for your access: " REPO_TOKEN
 
 read -p "Enter Container Name: " RUNNER_CONTAINER
 
-REPO_URL="git@github.com:Capson12/docker_runner.git"
 
 
-git clone --no-checkout $REPO_URL repo
-cd repo
-
-git sparse-checkout init --cone
-
-git sparse-checkout set "github/runners/$FOLDER_NAME"
-
-git checkout
-
-cd $FOLDER_NAME
-
-docker build -t $RUNNER_CONTAINER .
+docker build -t $RUNNER_CONTAINER dck-runner-builder/repo/$FOLDER_NAME
 
 docker run -d --name $REPO_NAME-$RUNNER_CONTAINER -e $REPO_NAME -e $REPO_TOKEN $RUNNER_CONTAINER
